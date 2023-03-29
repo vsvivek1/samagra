14038a210d7dd300e2fe8aa0a1909221b3182cdb
vivek <vs.vivek1@gmail.com>
2023-03-29 17:14:26 +0530

    intermediate

--- lib/screens/pol_var_screen.dart
+++ lib/screens/pol_var_screen.dart
@@ -23,17 +23,35 @@ class _PolVarScreenState extends State<PolVarScreen> {
   // List<String> _templates = [
 
   List<dynamic> _templates = [
-    'Template 1',
-    'Template 2',
-    'Template 3',
-    'Template 4',
-    'Template 5',
-    'Template 6',
-    'Template 7',
-    'Template 8',
-    'Template 9',
-    'Template 10',
+    {
+      "task_name":
+          "ReconductoringLT OH 5 wire line with LT three phase ABC(CIRCUIT KM)",
+      "structures": [
+        {"id": 3862, "structure_name": "COIL EARTH", "structure_code": 1113},
+        {
+          "id": 3870,
+          "structure_name": "ADD TRANSPORT TRIP",
+          "structure_code": 7207
+        }
+      ]
+    },
+    {
+      "task_name": "Another Task",
+      "structures": [
+        {
+          "id": 1234,
+          "structure_name": "Some Structure",
+          "structure_code": 5678
+        },
+        {
+          "id": 5678,
+          "structure_name": "Another Structure",
+          "structure_code": 9101
+        }
+      ]
+    }
   ];
+
   List<String> _selectedTemplates = [];
 
   Iterable<Map> getStructuresByName(d) {
@@ -42,10 +60,11 @@ class _PolVarScreenState extends State<PolVarScreen> {
     d.forEach((grp) {
       var e1 = (grp as Map)['mst_task'];
 
-      if (!e.contains(e1)) {
-        e.add(e1);
+      if (!e.contains(e1['task_name'])) {
+        e.add(e1['task_name']);
       }
     });
+
     var g2 = e.map((g) {
       var a = {};
 
@@ -61,11 +80,14 @@ class _PolVarScreenState extends State<PolVarScreen> {
       return a;
     });
 
+    print(g2);
+
+    print('g2 above');
+
     return g2;
   }
 
-  @override
-  void initState() {
+  Future _sheduleBuilder() {
     _fetchWorkDetails().then((workDetails) {
       print(workDetails.runtimeType);
 
@@ -75,33 +97,60 @@ class _PolVarScreenState extends State<PolVarScreen> {
 
       // print(workDetails[0]['wrk_schedule_group_structures']);
 
-      List wrk_schedule_group_structures =
+      List wrkScheduleGroupStructures =
           workDetails[0]['wrk_schedule_group_structures'];
 
-      var c = getStructuresByName(wrk_schedule_group_structures);
+      print(wrkScheduleGroupStructures);
+      print('wrkScheduleGroupStructures');
+      var c;
+      try {
+        c = getStructuresByName(wrkScheduleGroupStructures).toList();
 
-      // print(c);
+        // var clen = c.length;
+        // print(c);
 
-      setState(() {
-        _templates = c
-            .map((c1) {
-              c1['isExpanded'] = false;
-              var s = c1.toString();
-              print("$s is c1");
+        // for (int i = 0; i < c.length; i++) {
+        //   var a = c[i];
 
-              return c1;
-              // return c1["task_name"];
-            })
-            .toSet()
-            .toList();
+        //   print(i);
+        //   print(a);
+        // }
 
-        print(_templates);
-        print('te,mplatesabove');
-      });
+        return Future.value(c.toList());
+      } on Exception catch (e) {
+        c = [];
 
-      print('hi');
+        print(e);
+
+        return Future.value(c.toList());
+
+        // TODO
+      }
+
+      // print("this is c len $clen");
+      // setState(() {
+      //   _templates = c .toList();
+
+      //       // .map((c1) {
+      //       //   c1['isExpanded'] = false;
+      //       //   // var s = c1.toString();
+      //       //   // print("$s is c1");
+
+      //       //   return c1;
+      //       //   // return c1["task_name"];
+      //       // })
+      //       // .toSet()
+
+      //   // print(_templates.length);
+      //   // print('te,mplatesabove');
+      // });
+
+      // print('hi');
     });
+  }
 
+  @override
+  void initState() {
     // print(res);
     // ignore: todo
     // TODO: implement initState
@@ -110,138 +159,152 @@ class _PolVarScreenState extends State<PolVarScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppTheme.grey.withOpacity(0.7),
-        title: Text('Select Locations and Templates'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Number of Locations',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _numberOfLocations = int.tryParse(value) ?? 1;
-                });
-              },
+    return FutureBuilder(
+        future: _sheduleBuilder(),
+        builder: (context, AsyncSnapshot snapshot) {
+          print(snapshot.data);
+
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: AppTheme.grey.withOpacity(0.7),
+              title: Text('Select Locations and Templates'),
             ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: InputDecorator(
-                  decoration: InputDecoration(
-                    labelText: 'From Location',
-                    border: OutlineInputBorder(),
-                  ),
-                  child: TextField(),
-                ),
-              ),
-              SizedBox(width: 16),
-              Expanded(
-                child: InputDecorator(
-                  decoration: InputDecoration(
-                    labelText: 'To Location',
-                    border: OutlineInputBorder(),
-                  ),
-                  child: TextField(),
-                ),
-              ),
-              IconButton(
-                onPressed: saveFromAndTwoLocation,
-                icon: Icon(Icons.save),
-                color: Colors.grey,
-              )
-            ],
-          ),
-          Expanded(
-            child: Row(
+            body: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Expanded(
-                  flex: 1,
-                  child: GridView.builder(
-                    itemCount: _templates.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 1,
-                      childAspectRatio: 1.0,
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Number of Locations',
+                      border: OutlineInputBorder(),
                     ),
-                    itemBuilder: (BuildContext context, int index) {
-                      bool isExpanded = true; //_templates[index]['isExpanded'];
-                      // List<Map<String, dynamic>> tasks = _templates[index];
-                      //['tasks'];
-
-                      // 919656264570
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _templates[index]['isExpanded'] = !isExpanded;
-
-                            print(
-                                "this is expanded $_templates[index]['isExpanded']");
-
-                            // _selectedTemplates.add(_templates[index]);
-                          });
-                        },
-                        child: Container(
-                          margin: EdgeInsets.all(8.0),
-                          color: Colors.grey[300],
-                          child: Center(
-                            child: Column(
-                              children: [
-                                Text(_templates[index]),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      _templates[index]['isExpanded'] =
-                                          !isExpanded;
-                                    });
-                                  },
-                                  child: Text(
-                                    isExpanded ? 'Hide tasks' : 'Show tasks',
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      setState(() {
+                        _numberOfLocations = int.tryParse(value) ?? 1;
+                      });
                     },
                   ),
                 ),
-                Expanded(
-                  flex: 2,
-                  child: ListView.builder(
-                    itemCount: _numberOfLocations,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        margin: EdgeInsets.all(8.0),
-                        height: 50.0,
-                        color: Colors.grey[300],
-                        child: Center(
-                          child: Text(
-                            _selectedTemplates.length > index
-                                ? _selectedTemplates[index]
-                                : 'Select a template',
-                          ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: InputDecorator(
+                        decoration: InputDecoration(
+                          labelText: 'From Location',
+                          border: OutlineInputBorder(),
                         ),
-                      );
-                    },
+                        child: TextField(),
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: InputDecorator(
+                        decoration: InputDecoration(
+                          labelText: 'To Location',
+                          border: OutlineInputBorder(),
+                        ),
+                        child: TextField(),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: saveFromAndTwoLocation,
+                      icon: Icon(Icons.save),
+                      color: Colors.grey,
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: GridView.builder(
+                          itemCount: _templates.length,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1,
+                            childAspectRatio: 1.0,
+                          ),
+                          itemBuilder: (BuildContext context, int index) {
+                            bool isExpanded =
+                                true; //_templates[index]['isExpanded'];
+                            // List<Map<String, dynamic>> tasks = _templates[index];
+                            //['tasks'];
+
+                            // 919656264570
+
+                            // var cur = _templates[index];
+                            // var task = cur['task_name'];
+
+                            // print(task);
+                            // print('task above');
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  // _templates[index]['isExpanded'] = !isExpanded;
+
+                                  // print("this is expanded $task ");
+
+                                  // _selectedTemplates.add(_templates[index]);
+                                });
+                              },
+                              child: Container(
+                                margin: EdgeInsets.all(8.0),
+                                color: Colors.grey[300],
+                                child: Center(
+                                  child: ListView(
+                                    children: [
+                                      Text('task'),
+                                      IconButton(
+                                        icon: Icon(Icons.expand_less, size: 30),
+                                        onPressed: () {
+                                          setState(() {
+                                            // _templates[index]['isExpanded'] =
+                                            //     !isExpanded;
+                                          });
+                                        },
+                                        // icon: isExpanded
+                                        //     ? Icon(Icons.expand_less, size: 30)
+                                        //     : Icon(Icons.expand_more, size: 30),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: ListView.builder(
+                          itemCount: _numberOfLocations,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                              margin: EdgeInsets.all(8.0),
+                              height: 50.0,
+                              color: Colors.grey[300],
+                              child: Center(
+                                child: Text('hi'
+                                    // _selectedTemplates.length > index
+                                    //     ? _selectedTemplates[index]
+                                    //     : 'Select a template',
+                                    ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
+        });
   }
 
   Future<List<dynamic>> _fetchWorkDetails() async {
