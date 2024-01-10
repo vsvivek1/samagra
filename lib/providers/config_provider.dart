3fef9c209a852915a16543fb26936c109ddca940
vsvivek1 <vs.vivek1@gmail.com>
2024-01-10 11:32:55 +0530

    providers for app

---
+++ lib/providers/config_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:samagra/environmental_config.dart';
+
+class ConfigProvider extends ChangeNotifier {
+  late EnvironmentConfig _config;
+
+  EnvironmentConfig get config => _config;
+
+  Future<void> loadConfig() async {
+    try {
+      await dotenv.load(fileName: ".env");
+      _config = await EnvironmentConfig.fromEnvFile();
+      notifyListeners(); // Notify listeners about the new config
+    } catch (e) {
+      print("Error loading .env file: $e");
+      // Handle the error appropriately (throw/recover)
+    }
+  }
+}
